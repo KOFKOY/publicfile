92b97e4a5bebff5b9dce565f55186a275ef4920b
KOFKOY <1838039390@qq.com>
2025-03-05 15:12:06 +0800

    Update deployService.sh

--- deployService.sh
+++ deployService.sh
@@ -7,8 +7,8 @@ DEPLOY_PGSQL="true"
 DEPLOY_SPRINGBOOT="true"
 
 
-URL="https://ghfast.top/https://raw.githubusercontent.com/KOFKOY/publicfile/main/magic.sql" #要下载的SQL文件的URL
-URL_JAR="https://ghproxy.cc/https://github.com/KOFKOY/magic-api/releases/download/v1.8/magic.jar" #要下载的JAR包地址 有更新时其他不变，只需要修改版本号即可
+URL="https://raw.githubusercontent.com/KOFKOY/publicfile/main/magic.sql" #要下载的SQL文件的URL  SQL要在数据库创建成功后自己导入
+URL_JAR="https://github.com/KOFKOY/magic-api/releases/download/v1.8/magic.jar" #要下载的JAR包地址 有更新时其他不变，只需要修改版本号即可
 
 URL_FILE="magic.sql" # SQL文件名称
 
@@ -45,28 +45,29 @@ if [ "$DEPLOY_PGSQL" = "true" ]; then
 	  -e POSTGRES_DB="$DB" \
 	  -e POSTGRES_USER="$DB_USER" \
 	  -e POSTGRES_PASSWORD="$DB_PWD" \
-	  -v $(pwd)/"$URL_FILE":/docker-entrypoint-initdb.d/db.sql \
+	  -v /root/dbData/postgres:/var/lib/postgresql/data \
 	  -p 19003:5432 \
-	  1838039390/postgres2
-	echo "删除SQL文件"
-	rm "$URL_FILE"
+	   postgres
  
 fi
 
 
-
+echo "[DEBUG] 正在检查路径: $JAVA_JAR" >&2
+ls -l "$JAVA_JAR" || echo "文件不存在" >&2
 
 if [ "$DEPLOY_SPRINGBOOT" = "true" ]; then
+	
+	if [ ! -f "$JAVA_JAR" ]; then
+		echo "下载springboot项目jar包"
+			curl -L -o "$JAVA_JAR" "$URL_JAR"
 
-	echo "下载springboot项目jar包"
-        curl -L -o "$JAVA_JAR" "$URL_JAR"
-
-	# 检查 curl 命令是否成功执行
-	if [ $? -eq 0 ]; then
-	    echo "JAR下载成功."
-	else
-	    echo "JAR文件下载失败，请检查网络和URL"
-		exit 1
+		# 检查 curl 命令是否成功执行
+		if [ $? -eq 0 ]; then
+			echo "JAR下载成功."
+		else
+			echo "JAR文件下载失败，请检查网络和URL"
+			exit 1
+		fi
 	fi
 
 	 # 启动 springboot
@@ -83,6 +84,4 @@ if [ "$DEPLOY_SPRINGBOOT" = "true" ]; then
 		   -p 19004:"$SERVER_PORT" \
 		   openjdk:17-jdk-alpine \
 		   java -jar /app.jar
-	echo "删除JAR文件"
-	rm "$JAVA_JAR"
 fi
